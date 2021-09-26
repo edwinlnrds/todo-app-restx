d627156c81337d68ddefa71d63435e3c8f15cffe
Leonards03 <edwin.leonards03@gmail.com>
2021-09-26 18:22:11 +0700

    add alternatives route

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,10 @@ from app import api
 from app.controllers import AuthController, TodoController
 
 # Register route
-api.add_resource(AuthController.RegisterController, '/register')
+api.add_resource(AuthController.RegisterController, '/register', '/register/')
 # Login route
-api.add_resource(AuthController.AuthController, '/login')
+api.add_resource(AuthController.AuthController, '/login', '/login/')
 # Refresh token
-api.add_resource(AuthController.TokenRefreshController, '/refresh')
+api.add_resource(AuthController.TokenRefreshController, '/refresh', '/refresh/')
 # Todo Routes
-api.add_resource(TodoController.TodoController,'/todo', '/todo/<string:id>')
+api.add_resource(TodoController.TodoController,'/todo', '/todo/', '/todo/<string:id>')
