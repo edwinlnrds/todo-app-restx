2b54aa81a32a4633bdae8fbbdee8b63c936eb43e
Leonards03 <edwin.leonards03@gmail.com>
2021-10-04 17:30:49 +0700

    test: creating reusable fixture to shorten codes
    The list of added fixtures as follows:
    - make_user
    - user
    - other_user
    - make_todo
    - todo
    - register
    - login
    - get_token

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,16 +18,29 @@ def database(test_app):
         disconnect()
         connect('mongoenginetest', host='mongomock://localhost/mocking_db')
 
+@pytest.fixture
+def make_user():
+    def _make_user(without=[], with_empty=[]):    
+        payload = {
+            "name": "Edwin",
+            "email": "test@mail.com",
+            "password": "abcdefg",
+            "confirmation_password": "abcdefg"
+        }
+        
+        for keyword in without:
+            del payload[keyword]
+
+        for keyword in with_empty:
+            payload[keyword] = ""
+
+        return payload
+    return _make_user
+
 @pytest.fixture(scope="function")
-def user():
-    payload = {
-        "name": "Edwin",
-        "email": "test@mail.com",
-        "password": "abcdefg",
-        "confirmation_password": "abcdefg"
-    }
-    
-    return payload
+def user(make_user):
+    new_user =  make_user()
+    return new_user
 
 @pytest.fixture(scope="function")
 def other_user():
@@ -37,3 +50,76 @@ def other_user():
         "password": "12345678",
         "confirmation_password": "12345678"
     }
+
+@pytest.fixture(scope="function")
+def make_todo():
+    def _make_todo(without=[], with_empty=[]):
+        payload= {
+            "title": "Todo Payload",
+            "description": "test description",
+            "done": False
+        }
+        
+        for keyword in without:
+            del payload[keyword]
+
+        for keyword in with_empty:
+            payload[keyword] = ''
+
+        return payload
+    return _make_todo
+
+@pytest.fixture(scope="function")
+def todo(make_todo):
+    return make_todo()
+
+@pytest.fixture(scope="function")
+def register(client, database, user):
+    """
+    returns a function that can be called when 
+    register process is required
+    """
+    def _register(custom_user=None, expected_status_code=200):
+        """
+        By default, user already provided by fixtures above, method user()
+        
+        Paremeters:
+        custom_user can be passed, if we want to register with customized user
+        expected_status_code is for the code expected from the register process
+        """
+        if custom_user:
+            response = client.post('/register', json=custom_user)
+        else:
+            response = client.post('/register', json=user)
+        assert response.status_code == expected_status_code
+
+        import json
+        data = json.loads(response.get_data(as_text=True))
+        return data
+    return _register
+
+@pytest.fixture(scope="function")
+def login(client, database, user):
+    def _login(custom_user=None, expected_status_code=200):
+        if custom_user is not None:
+            response = client.post('/login', json=custom_user)
+        else:
+            response = client.post('/login', json=user)
+        assert response.status_code == expected_status_code
+
+        import json 
+        data = json.loads(response.get_data(as_text=True))
+        return data
+    return _login
+
+@pytest.fixture(scope="function")
+def get_token(register):
+    """
+    Return a user token that required for Authorization,
+    this fixture already include register process, and 
+    returned status code 200
+    """
+    def _get_token(user=None):
+        data = register(user)
+        return data['values']['token']['access_token']
+    return _get_token
