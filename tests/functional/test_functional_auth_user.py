d05cc38ab17ba5ed261d04db8cd11ed3504f181d
Leonards03 <edwin.leonards03@gmail.com>
2021-10-04 15:42:01 +0700

    refactor: shorten tests code using reusable user payload

--- tests/functional/test_functional_auth_user.py
+++ tests/functional/test_functional_auth_user.py
@@ -1,174 +1,101 @@
 import json
 
-def test_create_user(client, database):
-    payload = {
-        "name": "Edwin",
-        "email": "test@mail.com",
-        "password": "abcdefg",
-        "confirmation_password": "abcdefg"
-    }
-
-    response = client.post("/register", json=payload)
+def test_create_user(client, database, user):
+    response = client.post("/register", json=user)
 
     data = json.loads(response.get_data(as_text=True))
-    assert data["values"]["name"] == "Edwin"
+    assert data["values"]["name"] == user["name"]
 
-def test_create_user_twice(client, database):
-    payload = {
-        "name": "Edwin",
-        "email": "test@mail.com",
-        "password": "abcdefg",
-        "confirmation_password": "abcdefg"
-    }
-
-    response = client.post("/register", json=payload)
-    data = json.loads(response.get_data(as_text=True))
+def test_create_user_twice(client, database, user):
+    response = client.post("/register", json=user)
     assert response.status_code == 200
 
-    response = client.post("/register", json=payload)
+    response = client.post("/register", json=user)
     assert response.status_code == 400
 
 
-def test_create_user_with_empty_name(client, database):
-    payload = {
-        "name": "",
-        "email": "test@mail.com",
-        "password": "abcdefg",
-        "confirmation_password": "abcdefg"
-    }
+def test_create_user_with_empty_name(client, database, user):
+    user["name"] = ""
 
-    response = client.post("/register", json=payload)
-
+    response = client.post("/register", json=user)
     data = json.loads(response.get_data(as_text=True))
 
     assert response.status_code == 200
     assert data["values"]["name"] == ""
 
 
-def test_create_user_with_empty_email(client, database):
-    payload = {
-        "name": "Edwin",
-        "email": "",
-        "password": "abcdefg",
-        "confirmation_password": "abcdefg"
-    }
+def test_create_user_with_empty_email(client, database, user):
+    user["email"] = ""
 
-    response = client.post("/register", json=payload)
+    response = client.post("/register", json=user)
     assert response.status_code == 400
 
 
-def test_create_user_with_empty_password(client, database):
-    payload = {
-        "name": "Edwin",
-        "email": "test@mail.com",
-        "password": "",
-        "confirmation_password": "abcdefg"
-    }
+def test_create_user_with_empty_password(client, database, user):
+    user["password"] = ""
 
-    response = client.post("/register", json=payload)
+    response = client.post("/register", json=user)
     assert response.status_code == 400
 
 
-def test_create_user_with_empty_confirmation_password(client, database):
-    payload = {
-        "name": "Edwin",
-        "email": "test@mail.com",
-        "password": "abcdefg",
-        "confirmation_password": ""
-    }
+def test_create_user_with_empty_confirmation_password(client, database, user):
+    user["confirmation_password"] = ""
 
-    response = client.post("/register", json=payload)
+    response = client.post("/register", json=user)
     assert response.status_code == 400
 
 
-def test_create_user_without_password(client, database):
-    payload = {
-        "name": "User",
-        "email": "user@mail.com",
-        "confirmation_password": "abcdefgh"
-    }
-    response = client.post('/register', json=payload)
+def test_create_user_without_password(client, database, user):
+    del user["password"]
+    response = client.post('/register', json=user)
     assert response.status_code == 400
 
 
-def test_auth_user(client, database):
-    payload = {
-        "name": "Test",
-        "email": "test@mail.com",
-        "password": "12345678",
-        "confirmation_password": "12345678",
-    }
-    client.post('/register', json=payload)
+def test_auth_user(client, database, user):
+    client.post('/register', json=user)
 
-    payload = {
-        "email": "test@mail.com",
-        "password": "12345678",
-    }
-    response = client.post('/login', json=payload)
+    name = user["name"]
+    del user["name"]
+    del user["confirmation_password"]
+
+    response = client.post('/login', json=user)
     assert response.status_code == 200
 
     data = json.loads(response.get_data(as_text=True))
-    assert data['values']['name'] == "Test"
+    assert data['values']['name'] == name
     assert 'token' in data['values']
 
-def test_auth_user_with_wrong_email(client, database):
-    payload = {
-        "name": "Test",
-        "email": "test@mail.com",
-        "password": "12345678",
-        "confirmation_password": "12345678",
-    }
-    client.post('/register', json=payload)
+def test_auth_user_with_wrong_email(client, database, user):
+    client.post('/register', json=user)
 
-    payload = {
-        "email": "wrong.email@mail.com",
-        "password": "12345678",
-    }
-    response = client.post('/login', json=payload)
+    del user["name"]
+    del user["confirmation_password"]
+    user["email"] = "wrong_email@mail.com"
+
+    response = client.post('/login', json=user)
     assert response.status_code == 400
 
-def test_auth_user_with_wrong_password(client, database):
-    payload = {
-        "name": "Test",
-        "email": "test@mail.com",
-        "password": "12345678",
-        "confirmation_password": "12345678",
-    }
-    client.post('/register', json=payload)
+def test_auth_user_with_wrong_password(client, database, user):
+    client.post('/register', json=user)
 
-    payload = {
-        "email": "test@mail.com",
-        "password": "12345678910"
-    }
+    user["password"] = "wrongpassword"
 
-    response = client.post('/login', json=payload)
+    response = client.post('/login', json=user)
     assert response.status_code == 400
 
 
-def test_auth_user_with_empty_password(client, database):
-    payload = {
-        "name": "Test",
-        "email": "test@mail.com",
-        "password": "12345678",
-        "confirmation_password": "12345678"
-    }
-    client.post('/register', json=payload)
+def test_auth_user_with_empty_password(client, database, user):
+    client.post('/register', json=user)
 
-    payload = {
-        "email": "test@mail.com",
-        "password": ""
-    }
-    response = client.post('/login', json=payload)
+    del user["name"]
+    del user["confirmation_password"]
+    user["password"] = ""
+
+    response = client.post('/login', json=user)
     assert response.status_code == 400
 
-def test_auth_with_empty_payload(client, database):
-    payload = {
-        "name": "Test",
-        "email": "test@mail.com",
-        "password": "12345678",
-        "confirmation_password": "12345678"
-    }
-    client.post('/register', json=payload)
+def test_auth_with_empty_payload(client, database, user):
+    client.post('/register', json=user)
 
     payload = {}
     response = client.post('/login', json=payload)
